267a8a2ae1e6fff5fb97ba7dfd17a611e4fe8c95
Kate-delucki <60912896+Kate-delucki@users.noreply.github.com>
2020-07-24 23:32:48 +0700

    Add files via upload

---
+++ untitled folder/coding.js
@@ -0,0 +1,11 @@
+
+
+function myFunction() {
+    var x = document.getElementById("intro");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "block";
+      x.style.display = "none";
+    }
+  }
